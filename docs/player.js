e0e14e4cadd53f80cf383cc8bbc3217a67edcf36
Andres <47799692+AndresRuizB@users.noreply.github.com>
2019-11-03 18:12:49 +0100

    Actualizado movimiento diagonal
    Ahora la velocidad no aumenta cuando el tanque se mueve en diagonal

--- docs/player.js
+++ docs/player.js
@@ -1,30 +1,31 @@
 export default class Player extends Phaser.GameObjects.Container { //es un gameobject
-  constructor(scene, imag, x,y) {
-    let aspecto = scene.add.sprite(0,0,imag).setOrigin(0,0); //creas el sprite
+  constructor(scene, imag, x, y) {
+    let aspecto = scene.add.sprite(0, 0, imag).setOrigin(0, 0); //creas el sprite
     super(scene, x, y, aspecto);
     this.scene.add.existing(this); //le dice a la scene Game que existe
     this.scene.physics.add.existing(this); //¿le da físicas?
     this.body.setCollideWorldBounds(); //colisiona con los bordes de la partida
 
-    this.speed = 100;
+    this._maxSpeed = 100;
+    this.speed = this._maxSpeed;
     this.cursors = this.scene.input.keyboard.createCursorKeys(); //el cursor de los huevos
   }
 
 
   preUpdate() {
-    this.body.setVelocityY(0);
-    this.body.setVelocityX(0);
+    if ((this.cursors.up.isDown || this.cursors.down.isDown) && (this.cursors.left.isDown || this.cursors.right.isDown)) this.speed = this._maxSpeed * 71 / 100;
+    else this.speed = this._maxSpeed;
     if (this.cursors.up.isDown) {
       this.body.setVelocityY(-this.speed);
-    }
+    } else if (this.cursors.down.isDown) {
+      this.body.setVelocityY(this.speed);
+    }else       this.body.setVelocityY(0);
+
     if (this.cursors.left.isDown) {
       this.body.setVelocityX(-this.speed);
-    }
-    if (this.cursors.right.isDown) {
+    } else if (this.cursors.right.isDown) {
       this.body.setVelocityX(this.speed);
-    }
-    if (this.cursors.down.isDown) {
-      this.body.setVelocityY(this.speed);
-    }
+    }else this.body.setVelocityX(0);
+
   }
 }
