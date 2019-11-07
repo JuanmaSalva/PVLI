00c1bb57f7f48c05a7e72b20a58935eaebefa5a1
Andres <47799692+AndresRuizB@users.noreply.github.com>
2019-11-07 23:11:41 +0100

    Mejorada la rotacion con el movimiento del tanque

--- docs/tank.js
+++ docs/tank.js
@@ -3,18 +3,29 @@ export default class Tank extends Phaser.GameObjects.Sprite {
     super(scene, 0, -1, imag);
     this.scene.add.existing(this); //le dice a la scene Game que existe        
     this.parent = parent;
+    let angleObj, dir, oldAngle = 0;
   }
 
   preUpdate() {
+    this.dir = 0;
+    this.angleObj = 0;
+
     if (this.parent.cursors.up.isDown) {
-      this.angle = (0 + this.angle) / 2;
+      this.angleObj += this.oldAngle = 0;
+      this.dir += 1;
     } else if (this.parent.cursors.down.isDown) {
-      this.angle = (180 + this.angle) / 2;
+      this.angleObj += this.oldAngle = 180;
+      this.dir += 1;      
     }
     if (this.parent.cursors.right.isDown) {
-      this.angle = ((90 + this.angle) / 2);
+      this.angleObj += this.oldAngle = 90;
+      this.dir += 1;      
     } else if (this.parent.cursors.left.isDown) {
-      this.angle = -((270 + this.angle) / 2) - 90;
+      this.angleObj += this.oldAngle = 270;
+      this.dir += 1;
     }
+    if(this.angle = this.angleObj/(this.dir||1)){}
+    else this.angle = this.oldAngle||0;
+
   }
 }
