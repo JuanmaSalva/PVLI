0c2bb25fce424b94d1e6952970fc932d417e673e
Juanma <jsalva@ucm.es>
2019-11-18 11:47:38 +0100

    Nuevo Menu
    se ha cambiado la foto del menu a una un poco mas bonita aun q se sigue viendo mal

--- docs/menu.js
+++ docs/menu.js
@@ -6,10 +6,12 @@ export default class Menu extends Phaser.Scene {
 
     preload() {
         this.load.image('botonInicio', 'assets/botonStart.png');
+        this.load.image('fondoInicio', 'assets/fondoMenu.png');
     }
     
     create(){
-        this.boton =  this.add.image(450,300, 'botonInicio').setInteractive();
+        this.fondo = this.add.image(448,320,'fondoInicio'); //fondo provisional
+        this.boton =  this.add.image(448,268, 'botonInicio').setInteractive().setScale(0.5);
         this.boton.on('pointerdown', pointer =>{
             this.scene.start('main'); //main = el key de la escena Game
         })
