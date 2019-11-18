cde2b6ce3f8e9598b6133a1b3742b9d095f5b50c
Juanma <jsalva@ucm.es>
2019-11-18 12:01:43 +0100

    La ui da la vida se habia roto
    en algun momento en un merge se mantuvieron los dos .js del player y la ui no estaba en el correcto por tanto no funcionaba.
    ya esta arrelgado pero :whale:

--- docs/jugador/player.js
+++ docs/jugador/player.js
@@ -9,22 +9,47 @@ export default class Player extends Phaser.GameObjects.Container { //es un conta
     this.speed = this._maxSpeed;
     this.cursors = this.scene.input.keyboard.createCursorKeys();
     this.scena = scene;
+    this._maxLife = 100;
+    this.life = this._maxLife;
+
+    this.u = scene.input.keyboard.addKey('U'); //teclas provisionales para la vida en ui
+    this.j = scene.input.keyboard.addKey('J');
+    this.k = scene.input.keyboard.addKey('K');
 
     this.w = scene.input.keyboard.addKey('W');
     this.a = scene.input.keyboard.addKey('A');
     this.s = scene.input.keyboard.addKey('S');
     this.d = scene.input.keyboard.addKey('D');
 
-    this.b = scene.input.keyboard.addKey('B');
+    this.b = scene.input.keyboard.addKey('B'); //teclas provisionales para el cambio de arma
     this.n = scene.input.keyboard.addKey('N');
     this.m = scene.input.keyboard.addKey('M');
 
 
     this.armas = ['disparoSimple', 'rafagas', 'rebotador'];
     this.arma = this.armas[0]; //indica que arma tiene seleccionada
+
+    
+    this.dealDmg = function (damage) { //metodo en el que recibes daño
+      this.life -= damage;
+
+      if (this.life >= 0) {
+        console.log('Vida: ' + this.life);
+      } else {
+        this.life = 0;
+        console.log('Murio wey');
+      }
+    }
+
+    this.revive = function () { //se revive al juegador
+      this.life = this._maxLife;
+      console.log('1up');
+    }
   }
 
   preUpdate() {
+
+
     if ((this.cursors.up.isDown || this.cursors.down.isDown || this.s.isDown || this.w.isDown) && (this.cursors.left.isDown || this.cursors.right.isDown || this.s.isDown || this.w.isDown)) this.speed = this._maxSpeed * 71 / 100;
     else this.speed = this._maxSpeed;
     if (this.cursors.up.isDown || this.w.isDown) {
@@ -39,9 +64,16 @@ export default class Player extends Phaser.GameObjects.Container { //es un conta
       this.body.setVelocityX(this.speed);
     } else this.body.setVelocityX(0);
 
-    if (this.b.isDown) this.arma = this.armas[0];
-    else if (this.n.isDown) this.arma = this.armas[1];
+    if (this.b.isDown) this.arma = this.armas[0];//PROVISIONAL
+    else if (this.n.isDown) this.arma = this.armas[1];//PROVISIONAL
     else if (this.m.isDown) this.arma = this.armas[2];   //PROVISIONAL
+
+    if (this.u.isDown) this.dealDmg(-10);//PROVISIONAL
+    else if (this.j.isDown) this.dealDmg(10);//PROVISIONAL
+    else if (this.k.isDown) this.revive();//PROVISIONAL
+
+    
+    this.scena.lifeUI.setText('Life: '+this.life); //actualiza la ui
   }
 
   spawnBala = function () {
