9af76f1b72e4ed721a6949d4d238b014b1ee0cab
Juanma <jsalva@ucm.es>
2019-11-29 00:48:35 +0100

    no se si le he cagado con esto
    he cambiado unas pocas cosas para q no petara y sigue sin ir, si eso se hace un revert rico rico y ya, done

--- server.js
+++ server.js
@@ -12,23 +12,24 @@ app.get('/', function (req, res) {
 io.on('connection', socket => {
     console.log('a user connected');
     clients.push(socket);
-});
 
-socket.on('disconnect', () => {
-    console.log('a user disconnected');
-    clients.splice(clients.indexOf(socket), 1); // lo sacamos del array
-});
+    socket.on('disconnect', () => {
+        console.log('a user disconnected');
+        clients.splice(clients.indexOf(socket), 1); // lo sacamos del array
+    });
 
-socket.on('precios', mensaje => {
-    let lista =
-        mensaje === 'armas' ?
-            { espada: 400, escudo: 200 } :
-            { naranja: 10, limon: 15 };
 
-    // para enviar algo, usamos `emit`
-    // que tiene un nombre de mensaje,
-    // y un objeto
-    socket.emit('respuesta', lista);
+    socket.on('precios', mensaje => {
+        let lista =
+            mensaje === 'armas' ?
+                { espada: 400, escudo: 200 } :
+                { naranja: 10, limon: 15 };
+    
+        // para enviar algo, usamos `emit`
+        // que tiene un nombre de mensaje,
+        // y un objeto
+        socket.emit('respuesta', lista);
+    });
 });
 
 http.listen(port, () => {
