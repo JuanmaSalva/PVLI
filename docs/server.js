7d5d131cc02d82ec2f9b1bb193b641453bb15c9c
Juanma <jsalva@ucm.es>
2019-12-12 10:13:34 +0100

    Cambio al puerto 80
    Para q funcione en la facultad tiene que ser el puerto 80. Tmb la ip hay q camiarla cada vez que se quiera conectar

--- docs/server.js
+++ docs/server.js
@@ -3,7 +3,7 @@ const app = express(); // servidor de aplicaciones
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-const PORT = 3000;
+const PORT = 80;
 var clients = [];
 
 
