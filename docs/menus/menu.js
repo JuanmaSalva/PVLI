7d8bd02d99f3113371511f305360df0ca645e3b9
Juanma <jsalva@ucm.es>
2019-12-18 21:28:05 +0100

    Revert "Merge pull request #19 from JuanmaSalva/waiting"
    This reverts commit 61051e238c8f9399a748fbe24583e35194da98fc, reversing
    changes made to 7b43e712a74d85f3c9d04bc37caa75de56b91076.

--- docs/menus/menu.js
+++ docs/menus/menu.js
@@ -3,7 +3,7 @@ export default class Menu extends Phaser.Scene {
         super({ key: 'menu' });
     }
 
-    init(data) {
+    init(data){
         this.socket = data.s;
         this.numPlayer = data.numP;
     }
@@ -11,34 +11,21 @@ export default class Menu extends Phaser.Scene {
     preload() {
         this.load.image('botonPlay', 'assets/botonplay.png');
         this.load.image('fondoInicio', 'assets/fondoMenu.png');
-        this.load.image('onemoreneeded', 'assets/onemoreplayer.png')
 
         this.load.audio('click', 'assets/menuclick.wav');
     }
 
     create() {
         this.clickAudio = this.sound.add('click');
-
-        this.oneMore = true;
-
         this.add.image(448, 320, 'fondoInicio'); //fondo provisional
         this.boton = this.add.image(448, 268, 'botonPlay').setInteractive().setScale(0.7);
         this.boton.on('pointerdown', () => { //se le ha dado a jugar
             this.clickAudio.play();
-            if(this.oneMore) this.add.image(448, 450, 'onemoreneeded');
-
-            this.socket.emit('numeroDeJugadores');
+            this.socket.emit('numeroDeJugadores', this.numPlayer);
 
             this.socket.on('numeroDeJugadores', numero => {
-                if (numero == 2) {
-                    this.scene.start('menuArmas', { s: this.socket, numP: this.numPlayer }); //main = el key de la escena Game
-                }
-                else if (numero == 1) {
-                   this.oneMore = true;
-                }
-            })
-
-
+                if(numero == 2)this.scene.start('menuArmas',{s:this.socket, numP:this.numPlayer}); //main = el key de la escena Game
+            })            
         })
     }
 }
